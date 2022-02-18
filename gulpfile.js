f5b2b926cc65956d62dd6ec76b4dc068933dbfd0
bryant_huang <bryant6831@gmail.com>
2022-02-18 16:12:03 +0800

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const {src , dest} = require('gulp');
+
+const fileinclude = require('gulp-file-include');
+
+exports.html =  function includeHTML() {
+    return src('html/*.html')
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file'
+        }))
+        .pipe(dest('./'));
+}
