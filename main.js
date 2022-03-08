285f42579d709179a02d95f1ecc7581045dc329e
bryant_huang <bryant6831@gmail.com>
2022-03-08 11:36:33 +0800

    add

--- main.js
+++ main.js
@@ -6,7 +6,15 @@ console.log(sum(1,3));
 
 import $ from 'jquery';
 import './css/style.css';
-
+import gsap from 'gsap';
 
 $('body').css('background-color' , 'red');
+
+gsap.from('h1' , {
+   y: -150,
+   color: '#eee',
+   duration : 1,
+   opacity: 0
+})
+
 
