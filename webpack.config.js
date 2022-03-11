3543e60dac51ecb147cecae99dda5e2a2151c13b
bryant_huang <bryant6831@gmail.com>
2022-03-11 10:20:37 +0800

    add

--- webpack.config.js
+++ webpack.config.js
@@ -10,41 +10,68 @@ const webpack = require('webpack');
 
 module.exports = {
     entry: {
-        index: './main.js'
+        index: './main.js',
+        back : './back.js'
     },               // 入口文件
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'js/[name].js'
     },              // 出口文件
-    module: {
+   module: {
         rules: [{
             // 格式
             test: /\.(sass|scss|css)$/,
-            //順序是由下到上 sass > css > style
+            //順序是由下到上 css > style
             use: [{
-                loader: MiniCssExtractPlugin.loader,
-                options: {
-                    publicPath: './dist'
-                }
-            },
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                      publicPath: './dist'
+                    }
+                  },
+                // 'style-loader',//跟MiniCssExtractPlugin 會衝突所以要關掉
                 'css-loader',
                 'sass-loader'
             ],
-        }]
+        },
+        //babel loader
+        {
+            test: /\.(js)$/,
+            exclude: /(node_modules)/,
+
+            use: [{
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }],
+            include: path.resolve(__dirname, './'), //
+        },
+
+      ]
 
     },               // 處裡對應模組
     plugins: [
         //清理舊的檔案
         new CleanWebpackPlugin(),
         new MiniCssExtractPlugin({
-            filename: "./[name].css"
+            filename: "./css/[name].css"
         }),
+        //前台
         new HtmlWebpackPlugin({
             chunks: ['index'],  //選擇注入資源 chunk
             inject: 'body', //預設<body> js </body>  head or body
             template: './index.html',
             //來源
             filename: 'index.html'
+            // 目的地
+        }),
+        //後台
+         new HtmlWebpackPlugin({
+            chunks: ['back'],  //選擇注入資源 chunk
+            inject: 'body', //預設<body> js </body>  head or body
+            template: './back.html',
+            //來源
+            filename: 'back.html'
             // 目的地
         }),
         new webpack.ProvidePlugin({
